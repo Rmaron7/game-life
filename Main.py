16e2db3ad727c0ca73e2000885c67f2193e5e94a
Rmaron7 <fedor.kolerov@gmail.com>
2021-11-04 00:24:52 +0300

    InterfaceandBugfix

--- Main.py
+++ Main.py
@@ -8,13 +8,11 @@ fps_counter = 0
 
 clock = pygame.time.Clock()
 pygame.init()
-fps = 120
 fps_2 = int(5)
 screen = pygame.display.set_mode((1920, 1080))
 
 manager = pygame_gui.UIManager((1920, 1080))
 
-alive = False
 started = False
 
 buttonstart = pygame_gui.elements.UIButton(
@@ -39,13 +37,6 @@ buttonpause = pygame_gui.elements.UIButton(
     text='пауза'
     )
 
-buttonrestart = pygame_gui.elements.UIButton(
-    relative_rect=pygame.Rect(1800, 150, 120, 60),
-    manager=manager,
-    text='сброс'
-    )
-
-
 buttonquit = pygame_gui.elements.UIButton(
     relative_rect=pygame.Rect(1800, 60, 120, 60),
     manager=manager,
@@ -59,9 +50,40 @@ sliderfps = pygame_gui.elements.UIHorizontalSlider(
     manager=manager
     )
 
+buttonrestart = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect(1800, 150, 120, 60),
+    manager=manager,
+    text='сброс'
+    )
+
+sliderprobability = pygame_gui.elements.UIHorizontalSlider(
+    relative_rect=pygame.Rect(1800, 210, 120, 30),
+    start_value=0,
+    value_range=(0, 10),
+    manager=manager
+    )
+
+valueprobability = pygame_gui.elements.UILabel(
+    relative_rect=pygame.Rect(1800, 230, 120, 30),
+    text="",
+    manager=manager
+    )
+
+buttonstep = pygame_gui.elements.UIButton(
+    relative_rect=pygame.Rect(1800, 270, 120, 60),
+    manager=manager,
+    text='шаг'
+    )
+
+
+
 sliderfps.hide()
 buttonpause.hide()
 buttonrestart.hide()
+sliderprobability.hide()
+buttonstep.hide()
+valueprobability.hide()
+
 
 started = int(0)
 
@@ -73,13 +95,14 @@ field_updated = False
 #функция для генерации рандомных генов
 
 def gensrandom():
-    gensr =  [ random.choice([ 0, 1, 2, 3 ]) for z in range(9) ]
+    gensr =  [ random.choice([ 0, 1, 2, 3]) for z in range(9) ]
     for i in range (9):
         if gensr[i] != 1:
             gensr[i] = 0
-            gensr[3] = 0
-            gensr[2] = 0
-            gensr[4] = 0
+
+    gensr[2] = 0
+    gensr[3] = 0
+
     return gensr
 #-----------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
@@ -89,7 +112,11 @@ def near(pos: list,
          cellaround=[ [ -1, -1 ], [ -1, 0 ], [ -1, 1 ], [ 0, -1 ], [ 0, 1 ], [ 1, -1 ], [ 1, 0 ], [ 1, 1 ] ]):
     counternear = 0
     for i in cellaround:
-        counternear += cells[ (pos[ 0 ] + i[ 0 ]) % len(cells) ][ (pos[ 1 ] + i[ 1 ]) % len(cells[ 0 ]) ]
+
+        x = (pos[ 0 ] + i[ 0 ]) % len(cells)
+        y = (pos[ 1 ] + i[ 1 ]) % len(cells[ 0 ])
+
+        counternear += cells[ x ][ y ]
     return counternear
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -109,22 +136,25 @@ def gennear(pos: list,
     for i in range (9):
         j = random.choice([0, 1, 2])
         gensparents[i] = gens[parents[j][0]][parents[j][1]][i]
-        if random.randrange(1000) == 0:
+
+        chance = random.randrange(0, 10001)
+
+        if chance  < sliderprobability.get_current_value():
             gensparents[i] = 1
+
     return gensparents
 
 
 #-----------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
 
-
 mainrun = True
+
 while mainrun:
 
     fps_counter += 1
 
     time_delta = clock.tick(60) / 1000.0
-    #while started == 0:
 
     # обработка событий интерфейса
 
@@ -138,16 +168,16 @@ while mainrun:
                 if event.pos[0] < width*20 and event.pos[1] < height*20:
                     eventwidth = int(event.pos[0]//20)
                     eventheight = int(event.pos[1]//20)
-                    cellsn[eventheight][eventwidth] = 1
-                    gensn[eventheight][eventwidth] = gensrandom()
+                    cells[eventheight][eventwidth] = 1
+                    gens[eventheight][eventwidth] = gensrandom()
                     field_updated = True
 
             if event.button == 3:
                 if event.pos[0] < width*20 and event.pos[1] < height*20:
                     eventwidth = int(event.pos[0]//20)
                     eventheight = int(event.pos[1]//20)
-                    cellsn[eventheight][eventwidth] = 0
-                    gensn[eventheight][eventwidth] = [0]*9
+                    cells[eventheight][eventwidth] = 0
+                    gens[eventheight][eventwidth] = [0]*9
                     field_updated = True
 
         if event.type == pygame.USEREVENT:
@@ -157,24 +187,23 @@ while mainrun:
 
                 if event.ui_element == buttonpause:
                     started = -started
-                    fps = 120
+
+                if event.ui_element == buttonstep:
+                    started = 2
 
                 if event.ui_element == buttonrestart:
-                    cells = [[0 for j in range(width)] for i in range(height)]
+                    cells = [[random.choice([0, 1]) for j in range(width)] for i in range(height)]
                     cellsn = [[0 for j in range(width)] for i in range(height)]
                     gens = [[[0 for z in range(9)] for j in range(width)] for i in range(height)]
                     gensn = [[[0 for z in range(9)] for j in range(width)] for i in range(height)]
 
-                    cells = [[random.choice([0, 1]) for j in range(width)] for i in range(height)]
                     for i in range(len(cells)):
                         for j in range(len(cells[0])):
                             if cells[i][j] == 1:
                                 gens[i][j] = gensrandom()
 
-                    for i in range(len(cells)):
-                        for j in range(len(cells[ 0 ])):
-                            if cells[ i ][ j ] == 1:
-                                gens[ i ][ j ] = gensrandom()
+                    if (started != 1):
+                        started = 2
 
                 if event.ui_element == buttonstart:
                     if textwidth.get_text().isdigit():
@@ -197,13 +226,13 @@ while mainrun:
                         textheight.hide()
                         sliderfps.show()
                         buttonrestart.show()
+                        sliderprobability.show()
+                        buttonstep.show()
+                        valueprobability.show()
 
                         fps = 5
 
                         screen.fill('#000000')
-
-                        print("width=", width)
-                        print("height=", height)
 
                         cells = [ [ random.choice([ 0, 1 ]) for j in range(width) ] for i in range(height) ]
                         gens = [ [ [ 0 for z in range(9) ] for j in range(width) ] for i
@@ -231,58 +260,70 @@ while mainrun:
         if field_updated:
             for i in range(0, len(cells)):
                 for j in range(0, len(cells[ i ])):
-                    red = 85 * (gens[ i ][ j ][ 1 ] + gens[ i ][ j ][ 2 ] + gens[ i ][ j ][ 0 ])
-                    blue = 85 * (gens[ i ][ j ][ 7 ] + gens[ i ][ j ][ 8 ] + gens[ i ][ j ][ 6 ])
-                    green = 85 * (gens[ i ][ j ][ 4 ] + gens[ i ][ j ][ 5 ] + gens[ i ][ j ][ 3 ])
-                    cellcolor = (red, green, blue)
+                    if (cells [i][j] == 1):
+                        red = 60 + 65 * (gens[ i ][ j ][ 1 ] + gens[ i ][ j ][ 2 ] + gens[ i ][ j ][ 0 ])
+                        blue = 60 + 65 * (gens[ i ][ j ][ 7 ] + gens[ i ][ j ][ 8 ] + gens[ i ][ j ][ 6 ])
+                        green = 60 + 65 * (gens[ i ][ j ][ 4 ] + gens[ i ][ j ][ 5 ] + gens[ i ][ j ][ 3 ])
+                        cellcolor = (red, green, blue)
+                    else:
+                        cellcolor = (0,0,0)
                     pygame.draw.rect(screen, (cellcolor), [ j * 20, i * 20, 20, 20 ])
                 # отрисовка
 
-            pygame.display.update()
             field_updated = False
 
-        if started == 1 and not (fps_counter % fps_2): #работа
+        if ((started == 1) or (started == 2)) and (not (fps_counter % fps_2)): #работа
 
             field_updated = True
 
-
             for i in range(len(cells)):
+
                 for j in range(len(cells[ 0 ])):
-                    print(near([ i, j ]), cells[ i ][ j ]);
+
+                    current_cell_near = near([ i, j ])
+
                     if cells[ i ][ j ] == 1:
-                        current_cell_near = near([ i, j ])
+
+                        alive = False
+
                         for counter in range(9):
                             if gens[ i ][ j ][ counter ] == 1:
                                 if current_cell_near == counter:
                                     alive = True
+
                         if alive:
                             cellsn[ i ][ j ] = 1
                             gensn[ i ][ j ] = gens[ i ][ j ]
-                        if not alive:
+                        else:
                             cellsn[ i ][ j ] = 0
-                            gensn[ i ][ j ] = [ 0 ] * 9
+                            gensn[ i ][ j ] = [ 0  for i in  range(9)]
+
                         alive = False
                         # перепись живых и мертвых на живых
 
-                    if cells[ i ][ j ] == 0:
-                        if near([ i, j ]) == 3:
+                    else:
+                        if current_cell_near == 3:
                             cellsn[ i ][ j ] = 1
                             gensn[ i ][ j ] = gennear([i, j])
             # перепись живых и мертвых на мертвых
-            cells = cellsn
-            gens = gensn
+            for i in  range(len(cells)):
+                for j in range(len(cells[i])):
+                    cells[i][j] = cellsn[i][j]
+                    for z in range (9):
+                        gens[i][j][z] = gensn[i][j][z]
 
-#-----------------------------------------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------------------------------------
+            if (started == 2):
+                started = -1
 
-        if started ==-1 : #пауза
-            started = started
 
 #-----------------------------------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------------------------------
     if started != 0:
         fps_2 = 26 - sliderfps.get_current_value()
     #clock.tick(fps)
+
+    valueprobability.set_text(str((sliderprobability.get_current_value() / 100)))
+
     manager.update(time_delta)
     manager.draw_ui(screen)
     pygame.display.update()
